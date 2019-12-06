a67dc380e399cd93c9ae004716a893e606c78424
Olga Kukareko <olya.kukareko@gmail.com>
2019-12-06 16:07:31 +0300

    change function for slider , effect for hover

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-'use strict'
+'use strict';
 
-let arrow = document.getElementsByClassName('cards_page');
-let prev = document.getElementsByClassName('cards_prev');
-let next = document.getElementsByClassName('cards_next');
-let buttons = document.querySelectorAll('.gallery__item_link');
+let arrow = document.getElementsByClassName('cards_page'),
+    prev = document.querySelector('.cards_prev'),
+    next = document.querySelector('.cards_next'),
+    buttons = document.querySelectorAll('.gallery__item_link');
 
 let closes = document.querySelectorAll('.card__close');
 let popup = document.querySelector('.popup');
-let card = document.querySelector('.cards_block');
 
 
 buttons.forEach(button => button.addEventListener('click', function() {
     popup.classList.add('popup_active');
+    slider();
 }));
 
 closes.forEach(close => close.addEventListener('click', function() {
     popup.classList.remove('popup_active');
 }));
-let width = 1072;
-let count = 1;
 
-let block = cards.querySelector('.slider_block');
-let blockElem = cards.querySelectorAll('.cards_block');
+let sliderCards = document.querySelectorAll('.slider_block .cards_block');
+let current = 0;
 
-let position = 0;
-cards.querySelector('.cards_prev').onclick = function() {
-    position += width * count;
-    position = Math.min(position, 0)
-    block.style.marginLeft = position + 'px';
+function slider() {
+    for (let i = 0; i < sliderCards.length; i++) {
+        sliderCards[i].classList.add('slide_not-active');
+    }
+    sliderCards[current].classList.remove('slide_not-active');
 }
-cards.querySelector('.cards_next').onclick = function() {
-    position -= width * count;
-    position = Math.max(position, -width * (blockElem.length - count));
-    block.style.marginLeft = position + 'px';
+document.querySelector('.cards_prev').onclick = function() {
+    if (current - 1 == -1) {
+        current = sliderCards.length - 1;
+    } else {
+        current--;
+    }
+    slider();
+
+};
+document.querySelector('.cards_next').onclick = function() {
+    if (current + 1 == sliderCards.length) {
+        current = 0;
+    } else {
+        current++;
+    }
+    slider();
+
 };
